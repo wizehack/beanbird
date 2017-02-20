f548f12ea030a19bfe9ef233455ded49577cb0b4
Ubuntu <ubuntu@ip-172-31-20-230.us-west-2.compute.internal>
2017-02-20 16:09:12 +0900

    fix app.py bug

--- app.py
+++ app.py
@@ -6,25 +6,33 @@ from flask import make_response
 
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
 	req = request.get_json(silent=True, force=True)
-	print("Request:")
+	print("Get Request:")
 	print(json.dumps(req, indent=4))
+
 	res = processRequest(req)
 	print("Request processed")
+
 	res = json.dumps(res, indent=4)
 	print(res)
+
 	r = make_response(res)
 	print(r)
+
 	r.headers['Content-Type'] = 'application/json'
+
 	return r
+
 
 def processRequest(req):
 	if req.get("result").get("action") != "echoAction":
 		return {}
 	res = makeEchoResponse(req);
 	return res;
+
 
 def makeEchoResponse(req):
 	speech = req.get("result").get("resolvedQuery")
